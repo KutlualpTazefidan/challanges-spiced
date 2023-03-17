ff1b5a2eb205d0d7da2ddd093406587af34171f5
= <=>
2023-03-17 11:40:00 +0100

    First commit is missing

---
+++ 11_js-basics/js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,41 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+let title = "Title";
+let text = "Comments";
+let numberOfLikes = 0;
+let userName = "User Name";
+let isReported = true;
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log(
+  " Title:",
+  title,
+  "\n",
+  "Text:",
+  text,
+  "\n",
+  "Numer of likes:",
+  ++numberOfLikes,
+  "\n",
+  "User name:",
+  userName,
+  "\n",
+  "Reported:",
+  isReported
+);
+// --^-- write your code here --^--
