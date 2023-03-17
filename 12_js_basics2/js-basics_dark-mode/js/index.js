859e929d672bd0754d07a9386a3963a90b687a2f
= <=>
2023-03-17 16:02:53 +0100

    Cleaning the code

--- 12_js_basics2/js-basics_dark-mode/js/index.js
+++ 12_js_basics2/js-basics_dark-mode/js/index.js
@@ -4,17 +4,17 @@ const bodyElement = document.querySelector('[data-js="body"]');
 
 const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 console.log(darkModeButton);
-darkModeButton.addEventListener("click", (event) => {
+darkModeButton.addEventListener("click", () => {
   bodyElement.classList.add("body-dark");
 });
 
 const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
-lightModeButton.addEventListener("click", (event) => {
+lightModeButton.addEventListener("click", () => {
   bodyElement.classList.add("body-dark");
   bodyElement.classList.remove("body-dark");
 });
 
 const toggleButton = document.querySelector('[data-js="toggle-button"]');
-toggleButton.addEventListener("click", (event) => {
+toggleButton.addEventListener("click", () => {
   bodyElement.classList.toggle("body-dark");
 });
