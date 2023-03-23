85ba009154ccda6dce57867e3c417e1d604961e6
= <=>
2023-03-23 16:59:10 +0100

    JS Forms 2 Challenge - calculator - done

--- 19-forms2/js-forms_calculator/js/index.js
+++ 19-forms2/js-forms_calculator/js/index.js
@@ -23,9 +23,29 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let result;
-
   // --v-- write your code here --v--
-
+  const numberA = parseInt(document.getElementById("number-a").value);
+  const numberB = parseInt(document.getElementById("number-b").value);
+  const radioButtons = document.getElementsByName("operator");
+  for (i = 0; i < radioButtons.length; i++) {
+    console.log(radioButtons[i].checked);
+    if (radioButtons[i].checked) {
+      switch (radioButtons[i].value) {
+        case "addition":
+          result = add(numberA, numberB);
+          break;
+        case "subtraction":
+          result = subtract(numberA, numberB);
+          break;
+        case "multiplication":
+          result = multiply(numberA, numberB);
+          break;
+        case "division":
+          result = divide(numberA, numberB);
+          break;
+      }
+    }
+  }
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
