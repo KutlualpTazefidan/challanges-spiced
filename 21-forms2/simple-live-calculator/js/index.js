53191ac8e22dc48d14d5dce25dbd7bff17750ce8
= <=>
2023-03-24 16:09:03 +0100

    JS Forms 2-2 - Challenge - Live calculator - done

--- 21-forms2/simple-live-calculator/js/index.js
+++ 21-forms2/simple-live-calculator/js/index.js
@@ -3,3 +3,12 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+const resultsValue = 0;
+firstInput.addEventListener("input", (event) => {
+  result.textContent = event.target.value * secondInput.value;
+});
+
+secondInput.addEventListener("input", (event) => {
+  result.textContent = event.target.value * firstInput.value;
+});
