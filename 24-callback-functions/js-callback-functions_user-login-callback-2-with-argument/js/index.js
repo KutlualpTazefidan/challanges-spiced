63ad453e896ab83302b4541bde00e0864ba183da
= <=>
2023-03-29 10:10:53 +0200

    Callback Challenge - arguments - done

--- 24-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ 24-callback-functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,12 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+function showWelcomeMessage(userName) {
+  console.log(userName);
+}
+
+handleUserLogin(showWelcomeMessage, "Welcome Jane Doe! You are logged in now");
+
+handleUserLogin((userName) => {
+  console.log(userName);
+}, "Welcome Jane Doe! You are logged in now");
