5844409507f87a069213e907d4d9f39498f9aa2a
= <=>
2023-03-31 09:30:31 +0200

    quiz app challenge update

--- 27-export-function/js-structure_quiz-app/index.js
+++ 27-export-function/js-structure_quiz-app/index.js
@@ -1,12 +1,5 @@
 // Store the cards in a global state
-import { cards } from "./utils/cards.js";
-import { Header } from "./components/Header/Header.js";
-import { Form } from "./components/Form/Form.js";
-import { CardList } from "./components/CardList/CardList.js";
 
-const header = Header();
-const form = Form();
-CardList(cards);
 
 import { App } from "./components/App/App.js";
 
