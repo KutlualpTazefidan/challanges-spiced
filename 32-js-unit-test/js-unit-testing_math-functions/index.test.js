dfe75afb4a962be115ab82ec37755637c5333717
= <=>
2023-04-05 15:38:52 +0200

    Unit tests for subtract function

--- 32-js-unit-test/js-unit-testing_math-functions/index.test.js
+++ 32-js-unit-test/js-unit-testing_math-functions/index.test.js
@@ -16,3 +16,16 @@ test("Add 0.1 and 0.2", () => {
   const result = add(0.1, 0.2);
   expect(result).toBeCloseTo(0.3);
 });
+
+// Test cases for subtracting
+test("Subtract 5 from 15", () => {
+  const result = subtract(15, 5);
+  expect(result).toBe(10);
+});
+
+test("Second value greater than the first one", () => {
+  const result = subtract(0.1, 0.2);
+  expect(result).toBeLessThan(0);
+});
+
+//
