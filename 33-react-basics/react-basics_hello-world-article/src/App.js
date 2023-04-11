2605b4b68dfbc3d8157d43d8a8b3cf30e060048b
= <=>
2023-04-11 15:11:44 +0200

    react basics hello world article

--- 33-react-basics/react-basics_hello-world-article/src/App.js
+++ 33-react-basics/react-basics_hello-world-article/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./styles.css";
 
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello Tatooine</h1>
+      <p>We are back at Tatooine</p>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
 }
