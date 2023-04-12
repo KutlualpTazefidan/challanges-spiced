c7a77e4b469b6a564f03f51ed63735a44ad4d0ec
= <=>
2023-04-12 12:25:18 +0200

    Date of Today

--- 34-react-props/react-props_button/src/App.js
+++ 34-react-props/react-props_button/src/App.js
@@ -2,5 +2,37 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick(dateOfToday) {
+    alert(`Today is ${dateOfToday}!`);
+  }
+
+  return (
+    <Button
+      buttonColor="#ffffff"
+      buttonBackgroundColor="#fc0303"
+      disabled={false}
+      text="Date of Today !"
+      onPressButton={() => handleClick(new Date().toLocaleDateString("en-US"))}
+    />
+  );
+}
+
+function Button({
+  buttonColor,
+  buttonBackgroundColor,
+  disabled,
+  text,
+  onPressButton,
+}) {
+  const myButtonStyle = {
+    height: "2rem",
+    color: buttonColor,
+    backgroundColor: buttonBackgroundColor,
+  };
+
+  return (
+    <button onClick={onPressButton} style={myButtonStyle} disabled={disabled}>
+      {text}
+    </button>
+  );
 }
