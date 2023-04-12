959bd19648c3e5679b75438f4515669c395698cd
= <=>
2023-04-12 12:32:37 +0200

    Including all coaches

--- 34-react-props/react-props_greeting/src/App.js
+++ 34-react-props/react-props_greeting/src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 
 export default function App() {
-  return <Greeting name="Klaus" />;
+  return <Greeting name="Jan" />;
   // return <Greeting name="Ahsoka" />;
 }
 
 function Greeting({ name }) {
-  return <h1>Hello, {name === "Klaus" ? "Coach" : name}!</h1>;
+  const coaches = ["Klaus", "Gimena", "Marcell", "Yair", "Jan"];
+
+  return <h1>Hello, {coaches.includes(name) ? "Coach" : name}!</h1>;
 }
