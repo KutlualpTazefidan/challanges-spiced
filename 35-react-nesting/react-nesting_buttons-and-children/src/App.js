9eb8542309111dd04ba1f8083d27ee491aa0d54c
= <=>
2023-04-12 15:14:12 +0200

    Use components for the buttons

--- 35-react-nesting/react-nesting_buttons-and-children/src/App.js
+++ 35-react-nesting/react-nesting_buttons-and-children/src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Click me1!</Button>
+      <Button>Click me2!</Button>
+      <Button>Click me3!</Button>
+      <Button>Click me4!</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
