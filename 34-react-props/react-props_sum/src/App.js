2309e4a2c5ec1da02d3f80f98281379827bee206
= <=>
2023-04-12 10:57:39 +0200

    Sum some numbers

--- 34-react-props/react-props_sum/src/App.js
+++ 34-react-props/react-props_sum/src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={2} valueB={3} />;
+}
+function Sum({ valueA, valueB }) {
+  return (
+    <h1>
+      The sum of &quot;{valueA}&quot; and &quot;{valueB}&quot; is &quot;
+      {valueA + valueB}&quot;
+    </h1>
+  );
 }
