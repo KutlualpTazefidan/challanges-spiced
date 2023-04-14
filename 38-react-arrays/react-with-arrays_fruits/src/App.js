c8ff7dd1672eac40286745aeaa5830dfc7092350
= <=>
2023-04-14 10:26:19 +0200

    Display an array of fruits

--- 38-react-arrays/react-with-arrays_fruits/src/App.js
+++ 38-react-arrays/react-with-arrays_fruits/src/App.js
@@ -1,11 +1,39 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1338,
+      name: "🍎 Apple",
+      color: "red",
+    },
+    {
+      id: 1339,
+      name: "🥑 Avocado",
+      color: "green",
+    },
+    {
+      id: 1340,
+      name: "🍊 Orange",
+      color: "orange",
+    },
+    {
+      id: 1341,
+      name: "🥔 Potatoes",
+      color: "braun",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit, i) => {
+        return <Card name={fruit.name} color={fruit.color} key={i} />;
+      })}
     </div>
   );
 }
--- 38-react-arrays/react-with-arrays_fruits/src/components/Card.js
+++ 38-react-arrays/react-with-arrays_fruits/src/components/Card.js
@@ -1,5 +1,6 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ name, color }) {
+  const classWithColor = `card card--${color}`;
+  return <p className={classWithColor}>{name}</p>;
 }
