d270fd49acd75e519f0eaef19aedd504c7fdff8f
= <=>
2023-04-14 15:03:00 +0200

    Change counter using state

--- 39-react-forms/react-state-2_colored-number/src/components/Counter.js
+++ 39-react-forms/react-state-2_colored-number/src/components/Counter.js
@@ -1,15 +1,25 @@
 import ColoredNumber from "./ColoredNumber";
+import { useState } from "react";
 
 export default function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    setCounter(counter + 10);
+  }
+  function decrementCounter() {
+    setCounter(counter - 10);
+  }
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={counter} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={incrementCounter}
         >
           +
         </button>
@@ -17,6 +27,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={decrementCounter}
         >
           -
         </button>
