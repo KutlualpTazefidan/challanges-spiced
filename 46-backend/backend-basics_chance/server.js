98d06fdb3fbc5bb5dcb3f4b531972f3d784b54e4
= <=>
2023-05-04 11:40:09 +0200

    Randomize backend output

--- 46-backend/backend-basics_chance/server.js
+++ 46-backend/backend-basics_chance/server.js
@@ -1,3 +1,16 @@
 import { createServer } from "node:http";
+import Chance from "chance";
+const PORT = 8000;
+
+const chance = new Chance();
+export const server = createServer((req, res) => {
+  const namePerson = chance.name();
+  const agePerson = chance.age();
+  const professionPerson = chance.profession();
+  res.end(
+    `Hello, my name is ${namePerson} and I am ${agePerson} years old. I am a ${professionPerson}.`
+  );
+  res.statusCode = 200;
+});
 
 // export const server = …
