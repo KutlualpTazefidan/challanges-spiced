1b048058357e7abe0f340faaa5d3a5305f76cc2a
= <=>
2023-05-04 11:25:49 +0200

    Use different backend routes

--- 46-backend/backend-basics_routes/server.js
+++ 46-backend/backend-basics_routes/server.js
@@ -1,3 +1,28 @@
 import { createServer } from "node:http";
 
+export const server = createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      res.end("home");
+      res.statusCode = 200;
+      break;
+    case "/about":
+      res.end("about");
+      res.statusCode = 200;
+      break;
+    case "/api/fish/1":
+      res.end("shrimp");
+      res.statusCode = 200;
+      break;
+    case "/api/fish/2":
+      res.end("Anemonefish");
+      res.statusCode = 200;
+      break;
+    default:
+      res.end("not found");
+      res.statusCode = 404;
+      break;
+  }
+});
+
 // export const server = …
