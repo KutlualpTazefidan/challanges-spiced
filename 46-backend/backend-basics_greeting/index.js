6d69168c3ac48d52ab9c450504f8d5b5753e92b6
= <=>
2023-05-04 10:58:51 +0200

    Backend hello world

--- 46-backend/backend-basics_greeting/index.js
+++ 46-backend/backend-basics_greeting/index.js
@@ -1,3 +1,7 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
-// server.listen(…
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
