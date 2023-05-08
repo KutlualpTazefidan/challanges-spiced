ab795e66b86d256caf89b6084b5b53ae6619461a
= <=>
2023-05-08 11:50:15 +0200

    backend create challenge

---
+++ 46-backend/backend-create_products/components/ProductForm/index.js
@@ -0,0 +1,63 @@
+import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
+import { StyledButton } from "../Button/Button.styled";
+import useSWR from "swr";
+
+export default function ProductForm() {
+  const products = useSWR("/api/products");
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+    console.log("product-data", productData);
+    const response = await fetch("/api/products", {
+      method: "POST",
+      body: JSON.stringify(productData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // Here we're using fetch and not swr, because swr is for data fetching, and not data mutation.
+    // ... but we can notify swr about data changes using the mutate function! (See below.)
+    // Our method is post, the body contains our productData JSON, and our header provides additional information about the data we're sending.
+    console.log("response", response);
+
+    if (response.ok) {
+      await response.json();
+      // At this point, the promise of response has resolved.
+      products.mutate();
+      // Now we're notifying swr that our data has been mutated, which will trigger a rerender.
+      // If we don't include this line, the page won't automatically refresh and our submitted joke won't be immediately visible.
+      event.target.reset();
+    } else {
+      console.error(`Error: ${response.status}`);
+    }
+  }
+
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledHeading>Add a new Fish</StyledHeading>
+      <StyledLabel htmlFor="name">
+        Name:
+        <input type="text" id="name" name="name" />
+      </StyledLabel>
+      <StyledLabel htmlFor="description">
+        Description:
+        <input type="text" id="description" name="description" />
+      </StyledLabel>
+      <StyledLabel htmlFor="price">
+        Price:
+        <input type="number" id="price" name="price" min="0" />
+      </StyledLabel>
+      <StyledLabel htmlFor="currency">
+        Currency:
+        <select id="currency" name="currency">
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+        </select>
+      </StyledLabel>
+      <StyledButton type="submit">Submit</StyledButton>
+    </StyledForm>
+  );
+}
